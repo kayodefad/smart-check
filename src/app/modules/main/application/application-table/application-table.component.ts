9391263b378bff3c45b0c2d5a815081c1ce31212
Kayode Fadoju <kayode.fadoju@thriveagric.com>
2022-09-23 09:38:31 +0100

    completed application module

---
+++ src/app/modules/main/application/application-table/application-table.component.ts
@@ -0,0 +1,249 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-application-table',
+  templateUrl: './application-table.component.html',
+  styleUrls: ['./application-table.component.scss'],
+})
+export class ApplicationTableComponent implements AfterViewInit {
+  displayedColumns: string[] = [
+    'serial_number',
+    'app_name',
+    'app_type',
+    'app_dcsc',
+    'app_port',
+    'end_point',
+    'host',
+    'action',
+  ];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selectedStatus: number = 1;
+
+  statuses = [
+    { id: 1, name: 'All' },
+    { id: 2, name: 'Active' },
+    { id: 3, name: 'Inactive' },
+  ];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+}
+
+export interface PeriodicElement {
+  serial_number: number;
+  app_name: string;
+  app_type: string;
+  app_dcsc: string;
+  app_port: string;
+  end_point: string;
+  host: string;
+  action: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    action: '',
+    serial_number: 1,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 2,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 3,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 4,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 5,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 6,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 7,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 8,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 9,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 10,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 11,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 12,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 13,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 14,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 15,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 16,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 17,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 18,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 19,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+  {
+    action: '',
+    serial_number: 20,
+    app_name: 'Application name goes here',
+    app_type: 'Application type',
+    app_dcsc: 'Application type',
+    app_port: 'Application port',
+    end_point: 'End point',
+    host: '10.1.101.53',
+  },
+];
--- src/app/modules/main/application/application.module.ts
+++ src/app/modules/main/application/application.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ApplicationComponent } from './application.component';
 import { RouterModule } from '@angular/router';
 import { ROUTES } from './application.routes';
+import { SharedModule } from '../../shared/shared.module';
+import { ApplicationTableComponent } from './application-table/application-table.component';
 
 @NgModule({
-  declarations: [ApplicationComponent],
-  imports: [CommonModule, RouterModule.forChild(ROUTES)],
+  declarations: [ApplicationComponent, ApplicationTableComponent],
+  imports: [CommonModule, SharedModule, RouterModule.forChild(ROUTES)],
 })
 export class ApplicationModule {}
